8979106f20cefffa1728087981d8ccdf31648257
Joun Cato <jouncato@gmail.com>
2020-06-05 11:11:32 -0500

    Actualización feature001

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Se realiza cambio feature001
         </a>
       </header>
     </div>
